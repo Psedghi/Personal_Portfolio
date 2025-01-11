4795f7b791065d88d57c566e88eec4e939d9ecd8
Parsa Sedghi <psedghi@umd.edu>
2025-01-10 23:24:28 -0500

    updated this

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -259,7 +259,7 @@ export default function Projects() {
             <div className="relative w-2/3 md:w-2/5"> {/* Adjusted width to 2/5 */}
               <div className="bg-[#4C230A] rounded-lg overflow-hidden shadow-lg hover:scale-105 transition-transform duration-300">
                 <img
-                  src="/.hylight.png"
+                  src="./hylight.png"
                   alt="HyLight"
                   className="w-full h-auto"
                 />
